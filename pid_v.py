bf1ebfdc64e5cdb789bc0061d0f38e6cf5985047
Qiyuan Wu <qwu312@gatech.edu>
2020-06-21 16:14:31 -0600

    added update method

--- pid_v.py
+++ pid_v.py
@@ -12,7 +12,7 @@ class pid(object):
         i_t (float): integral time.
         d_t (float): derivative time.
         All p, i, and d must be positive.
-        loss (float): loss constant.
+        loss (float): loss constant, must be negative.
         '''
         if p < 0.0 or i_t < 0.0 or d_t < 0.0:
             raise ValueError("p, i, and d must be positive")
@@ -26,9 +26,9 @@ class pid(object):
         self.i_gain = p / i_t
         self.d_gain = p * d_t
     
-    def run(self, sp_i, run_t, ramp=0):
+    def run(self, sp_i=50, run_t=200, ramp=0):
         '''
-        Simulate a simple PID heating run.
+        Simulate a simple PID heating run, assuming the lowest temperature is 0.
         sp_i (float): initial setpoint temperature.
         run_t (int): running time in seconds.
         ramp (float): ramp rate in degrees per second.
@@ -57,8 +57,34 @@ class pid(object):
             i_o = self.i_gain * np.sum(self.errors)
             d_o = self.d_gain * (self.errors[i] - self.errors[i-1])
             outputs = np.max((p_o + i_o + d_o, 0))
-            measure_i = self.measured[i] + outputs + self.loss
+            measure_i = np.max((self.measured[i] + outputs + self.loss, 0))
             self.measured.append(measure_i)
+    
+    def update(self, p, i_t, d_t, loss, sp_i=50, run_t=200, ramp=0):
+        '''
+        Update the parameters and run again.
+        p (float): proportional coefficient.
+        i_t (float): integral time.
+        d_t (float): derivative time.
+        All p, i, and d must be positive.
+        loss (float): loss constant, must be negative.
+        sp_i (float): initial setpoint temperature.
+        run_t (int): running time in seconds.
+        ramp (float): ramp rate in degrees per second.
+        '''
+        if p < 0.0 or i_t < 0.0 or d_t < 0.0:
+            raise ValueError("p, i, and d must be positive")
+        if loss >=0:
+            raise ValueError("loss must be negative")
+        # update the parameters
+        self.p_gain = p
+        self.i_time = i_t
+        self.d_time = d_t
+        self.loss = loss
+        self.i_gain = p / i_t
+        self.d_gain = p * d_t
+        # run again
+        self.run(sp_i=sp_i, run_t=run_t, ramp=ramp)
 
 if __name__ == '__main__':
     pid_1 = pid(10.0, 10.0, 2.0)
